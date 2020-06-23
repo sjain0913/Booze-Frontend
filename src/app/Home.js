4568949940e88b448480af2ae7fc3faa5bdc7d21
Saumya Jain <samjain0913@gmail.com>
2020-06-23 18:28:57 +0530

    link on main page

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './styles/Home.css';
-import { Helmet } from 'react-helmet';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
     render() {
@@ -11,7 +11,7 @@ class Home extends Component {
                     <br></br>
                     <p>App currently in development!</p>
                     
-                <a href="/api">Link to Booze API</a>
+                    <Link to="/api">Link to Booze API</Link>
                 </div>
             </div>
         )
